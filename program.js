27e40d7d663f94610a03115e268b4b3a7925e12c
Caroline Marsden <car.marsden@gmail.com>
2019-11-25 17:11:03 -0500

    Retrieve & parse input json file from path

--- program.js
+++ program.js
@@ -1,8 +1,16 @@
 const readline = require('readline');
+const fs = require('fs');
 
+// retrieve file & parse it to 'data' obj
 const filePath = process.argv.slice(2)[0];
 console.log(filePath);
 
+const jsonString = fs.readFileSync(filePath, 'utf8');
+const data = JSON.parse(jsonString);
+console.log(data.subviews[0].subviews[0].subviews);
+
+
+// listen for input
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
